cf960efb20ea9a7e3589b49f9b5961ef269a6eae
Kevin Laude <nerfyoda@gmail.com>
2019-10-31 12:04:48 -0500

    Remove an unnecessary error wrap

--- stackpath/errors.go
+++ stackpath/errors.go
@@ -9,13 +9,11 @@ import (
 // InvalidClientSecretError models when a StackPath API OAuth 2 client ID is
 // invalid, either due to an invalid format or because the client ID does not
 // exist at StackPath.
-type InvalidClientIDError struct {
-	Err error
-}
+type InvalidClientIDError struct{}
 
 // NewInvalidClientIDError wraps an existing error as an invalid client ID error.
-func NewInvalidClientIDError(err error) *InvalidClientIDError {
-	return &InvalidClientIDError{Err: err}
+func NewInvalidClientIDError() *InvalidClientIDError {
+	return &InvalidClientIDError{}
 }
 
 // Error returns a human-readable invalid client ID error message.
@@ -25,14 +23,12 @@ func (e *InvalidClientIDError) Error() string {
 
 // InvalidClientSecretError models when a StackPath API OAuth 2 client ID is
 // correct, but the client secret is incorrect.
-type InvalidClientSecretError struct {
-	Err error
-}
+type InvalidClientSecretError struct{}
 
 // NewInvalidClientSecretError wraps an existing error as an invalid client
 // secret error.
-func NewInvalidClientSecretError(err error) *InvalidClientSecretError {
-	return &InvalidClientSecretError{Err: err}
+func NewInvalidClientSecretError() *InvalidClientSecretError {
+	return &InvalidClientSecretError{}
 }
 
 // Error returns a human-readable invalid client secret error message.
@@ -52,11 +48,11 @@ func NewStackPathError(err error) error {
 			// A 401 Unauthorized error means the client ID was valid, but the
 			// corresponding secret wasn't.
 			case 401:
-				return NewInvalidClientSecretError(err)
+				return NewInvalidClientSecretError()
 
 			// A 404 Not Found error means the client ID didn't exist.
 			case 404:
-				return NewInvalidClientIDError(err)
+				return NewInvalidClientIDError()
 			}
 		}
 	}
