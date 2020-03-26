94605df083c3d15cbc6a650849973ab8fc60cc9c
Kevin Laude <nerfyoda@gmail.com>
2020-03-26 14:03:43 -0500

    Send more customized error messages to users
    Forego the string representation of the underlying go-swagger error in
    favor of inspected and customized error messages to the users. Present
    the HTTP status code, the error message, a list of field violations, and
    the API request ID to the user.

--- stackpath/errors_test.go
+++ stackpath/errors_test.go
@@ -6,6 +6,8 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/terraform-providers/terraform-provider-stackpath/stackpath/api/ipam/ipam_client/network_policies"
+	"github.com/terraform-providers/terraform-provider-stackpath/stackpath/api/ipam/ipam_models"
 	"golang.org/x/oauth2"
 )
 
@@ -58,5 +60,125 @@ func TestBuildANonStackPathError(t *testing.T) {
 	case *TestError:
 	default:
 		t.Errorf("NewStackPathError built an incorrect error type. Expected TestError but got %t", err)
+	}
+}
+
+func TestBuildAnAPIError(t *testing.T) {
+	expectedStatusCode := 500
+	expectedMessage := "Internal Server Error"
+	expectedRequestID := "deadbeef"
+	expectedFullError := "a 500 error was returned from StackPath: \"Internal Server Error\" (request ID deadbeef)"
+
+	result := network_policies.NewCreateNetworkPolicyInternalServerError()
+	result.Payload = &ipam_models.APIStatus{
+		Code:    13,
+		Message: expectedMessage,
+	}
+
+	result.Payload.SetDetails([]ipam_models.APIStatusDetail{
+		&ipam_models.StackpathRPCRequestInfo{
+			RequestID: expectedRequestID,
+		},
+	})
+
+	err := NewStackPathError(result)
+
+	switch err.(type) {
+	case *APIError:
+	default:
+		t.Errorf(
+			"NewStackPathError built an incorrect error type. Expected APIError but got %T",
+			err,
+		)
+	}
+
+	if err.(*APIError).statusCode != expectedStatusCode {
+		t.Errorf(
+			"NewStackPathError built an APIError with an incorrect statusCode. Expected %d but got %d",
+			expectedStatusCode,
+			err.(*APIError).statusCode,
+		)
+	}
+
+	if err.(*APIError).message != expectedMessage {
+		t.Errorf(
+			"NewStackPathError built an APIError with an incorrect message. Expected \"%s\" but got \"%s\"",
+			expectedMessage,
+			err.(*APIError).message,
+		)
+	}
+
+	if err.(*APIError).requestID != expectedRequestID {
+		t.Errorf(
+			"NewStackPathError built an APIError with an incorrect requestID. Expected \"%s\" but got \"%s\"",
+			expectedRequestID,
+			err.(*APIError).requestID,
+		)
+	}
+
+	if err.(*APIError).Error() != expectedFullError {
+		t.Errorf(
+			"NewStackPathError built an APIError that rendered an incorrect error message. Expected \"%s\" but got \"%s\"",
+			expectedFullError,
+			err.(*APIError).Error(),
+		)
+	}
+}
+
+func TestBuildAnAPIErrorWithNoRequestID(t *testing.T) {
+	expectedFullError := "a 500 error was returned from StackPath: \"Internal Server Error\""
+
+	result := network_policies.NewCreateNetworkPolicyInternalServerError()
+	result.Payload = &ipam_models.APIStatus{
+		Code:    13,
+		Message: "Internal Server Error",
+	}
+
+	err := NewStackPathError(result)
+
+	if err.(*APIError).Error() != expectedFullError {
+		t.Errorf(
+			"NewStackPathError built an APIError that rendered an incorrect error message. Expected \"%s\" but got \"%s\"",
+			expectedFullError,
+			err.(*APIError).Error(),
+		)
+	}
+}
+
+func TestBuildAnAPIErrorWithFieldViolations(t *testing.T) {
+	expectedFullError := "a 400 error was returned from StackPath: \"network policy with priority 20000 already exists on stack\". The following fields have errors: foo: is required, bar: must be less than three (request ID deadbeef)"
+
+	result := network_policies.NewCreateNetworkPolicyDefault(400)
+	result.Payload = &ipam_models.APIStatus{
+		Code:    3,
+		Message: "network policy with priority 20000 already exists on stack",
+	}
+
+	result.Payload.SetDetails([]ipam_models.APIStatusDetail{
+		&ipam_models.StackpathRPCRequestInfo{
+			RequestID: "deadbeef",
+		},
+		&ipam_models.StackpathRPCBadRequest{
+			FieldViolations: []*ipam_models.StackpathRPCBadRequestFieldViolation{
+				{
+					Field:       "foo",
+					Description: "is required",
+				},
+				{
+					Field:       "bar",
+					Description: "must be less than three",
+				},
+			},
+		},
+	})
+
+	err := NewStackPathError(result)
+
+	if err.(*APIError).Error() != expectedFullError {
+		t.Errorf(
+			"NewStackPathError built an APIError that rendered an incorrect error message. Expected \"%s\" but got \"%s\"",
+			expectedFullError,
+			err.(*APIError).Error(),
+		)
 	}
 }
